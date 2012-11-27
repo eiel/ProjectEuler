fe3321addd720134cfc6e3bd481616312f0d9f2f
ryosms <ryo.sms@gmail.com>
2012-11-27 17:09:33 +0900

    Add Problem007.py
    Solve Problem 7 (Find the 10001st prime.)

---
+++ python/Problem007.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import sys
+
+primes = []
+
+def check_prime(val) :
+	for num in primes :
+		if val % num == 0 :
+			return 0
+
+	primes.append(val)
+	return 1
+
+if __name__ == '__main__' :
+	
+	max = 10
+	if len(sys.argv) > 1 and sys.argv[1].isdigit() :
+		max = int(sys.argv[1])
+
+	val = 2
+	while 1 :
+		if check_prime(val) :
+			if len(primes) == max :
+				print val
+				break
+
+		val += 1
+
+
+
+
+
+
