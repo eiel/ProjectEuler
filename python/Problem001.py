e6a58d4a2622df7c93c37507b77e8b0fe45df6d7
ryosms <ryo.sms@gmail.com>
2012-11-27 11:37:59 +0900

    Add Problem001.py
    Solve Problem 1 (Add all the natural numbers below one thousand that are multiples of 3 or 5.)

---
+++ python/Problem001.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import sys
+
+def sum_multiple_of_3_or_5(max) :
+	result = 0
+	for i in range(1, max) :
+		if i % 3 == 0 or i % 5 == 0 :
+			result += i
+
+	return result
+
+if __name__ == '__main__' :
+	max = 10
+	if len(sys.argv) > 1 and sys.argv[1].isdigit() :
+		max = int(sys.argv[1])
+
+	print sum_multiple_of_3_or_5(max)
+
