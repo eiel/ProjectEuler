fe94e522cccd76ecdbe7a9295d3241bde461e51c
ryosms <ryo.sms@gmail.com>
2012-11-27 15:19:39 +0900

    Add Problem005.py
    Solve Problem 5 (What is the smallest number divisible by each of the numbers 1 to 20?)

---
+++ python/Problem005.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import sys
+
+def factorize_of_prime(num, list) :
+	for val in list :
+		if num % val == 0 :
+			num = num / val;
+
+	if num != 1 :
+		list.append(num)
+
+if __name__ == '__main__':
+	max = 10
+	if len(sys.argv) > 1 and sys.argv[1].isdigit() :
+		max = int(sys.argv[1])
+
+	list = [1]
+	for i in range(1, max + 1) :
+		factorize_of_prime(i, list)
+
+	val = 1
+	for num in list :
+		val *= num
+
+	print val
+
+
